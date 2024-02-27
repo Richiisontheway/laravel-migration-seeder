a3afb20ca90cef27920691b88fbfc046be45e581
Richiisontheway <mineiriccardo@gmail.com>
2024-02-27 18:01:05 +0100

    general update

--- app/Http/Controllers/treni/TrainController.php
+++ app/Http/Controllers/treni/TrainController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers\treni;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+//models
+use  App\Models\Train;
 class TrainController extends Controller
 {
-    //
+    public function index(){
+        $trains = Train::all();
+        return view('welcome',compact('trains'));
+    }
+    // public function show($id){
+    //     $movie = Train::where('id',$id)->first();
+    //     return view('movies.show',compact('movie'));
+    // }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,17 @@
 @section('page-title', 'Home')
 
 @section('main-content')
-<h1>
-    Laravel Start 1
-</h1>
-
-<h2>
-    Ciao {{ $firstName }} {{ $lastName }}
-</h2>
+<div class="row">
+        @foreach ($trains as $train)
+            <div class="col-12 col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h3>
+                            {{ $train->factory }}
+                        </h3>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
